558bb9d49cc4530b368111725e1acd0e1b8c0849
a.sumin <sumin93@mail.ru>
2022-04-18 23:35:10 +0300

    Creating our own coroutine scope

--- app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
@@ -1,15 +1,15 @@
 package com.sumin.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default + CoroutineName("My coroutine scope"))
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
@@ -21,21 +21,22 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        coroutineScope.launch(Dispatchers.Main) {
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
             _state.value = Factorial(result)
+            Log.d("MainViewModel", coroutineContext.toString())
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
     }
 
 //    private suspend fun factorial(number: Long): String {
@@ -49,4 +50,9 @@ class MainViewModel : ViewModel() {
 //            }
 //        }
 //    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
 }
